2dbb4c88481b33e9c95aadfb00d675d01622dee0
Caio Filipini <caiofilipini@gmail.com>
2011-11-05 18:33:42 -0200

    improved spec clarity

--- spec/security/hash_key_spec.rb
+++ spec/security/hash_key_spec.rb
@@ -3,7 +3,13 @@ require "security/hash_key"
 
 describe HashKey do
 
-  subject { HashKey.new(:first => "i_am_number_one", :second => "and_i_am_number_two").compute }
-  
-  it { should == "10f76998e22c989efb6544b2f25aaf3b7c6946f7" }
+  describe "#compute" do
+    let(:hash_key) {
+      HashKey.new(:first => "i_am_number_one", :second => "and_i_am_number_two")
+    }
+
+    subject { hash_key.compute }
+    it { should == "10f76998e22c989efb6544b2f25aaf3b7c6946f7" }
+  end
+
 end
